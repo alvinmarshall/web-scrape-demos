d0a61652442a38947ec112f47ce3fe970ab4f942
alvinmarshall <kelvinbirikorang@gmail.com>
2024-01-27 11:52:05 -0500

    chore: high risk countries

---
+++ high-risk-third-countries-scrape.py
@@ -0,0 +1,15 @@
+from bs4 import BeautifulSoup as soup
+from bs4 import ResultSet
+
+f = open("html/high-risk-third-countries.html", "r")
+
+page_soup = soup(f.read(),"html.parser")
+field_contents: ResultSet = page_soup.findAll("div",{"class":"field-content"})
+content_arr = []
+for field_content in field_contents:
+    content = field_content.select("ul:not(:has(li a)) li")
+    content_arr = content
+countries = [li.get_text().replace('*','') for li in content_arr]
+
+print(countries)
+print(len(countries))
